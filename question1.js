e702ddbc450b5991ec9cc25d70b4e890b3a77302
MohdWaseem0786 <mohdv2818@gmail.com>
2024-02-24 16:07:26 +0530

    write for loop to log each element multiply by 2

--- question1.js
+++ question1.js
@@ -1,1 +1,15 @@
-let arr=[1,2,3,4,5,6,7,8,9,10]
+// let arr=[1,2,3,4,5,6,7,8,9,10]
+//write for loop to log each element multiply by 2
+const arr=[1,2,3,6,4,5]
+const array=[1,2,3,6,4,5,6]
+let newarr=[]
+
+const fn =(a)=>{
+    for(let i=0;i<a.length;i++){
+        console.log(a[i]*2);
+    }
+}
+ 
+fn(array)
+//result
+// [2,4,6,8,10]
