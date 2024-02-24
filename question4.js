a59f691ddde56b94978a129bfa90038b400cebf9
MohdWaseem0786 <mohdv2818@gmail.com>
2024-02-24 17:29:10 +0530

    write a for loop to find and log the smallest element in the array

---
+++ question4.js
@@ -0,0 +1,16 @@
+//write a for loop to find and log the smallest element in the array
+
+
+let arr=[10,2,30,4,50]
+
+const fn=(a)=>{
+    let max=a[0]
+    for(let i=0;i<a.length;i++){
+       if (max>a[i]) {
+        max=a[i]
+       }
+    }
+    return max
+}
+
+console.log(fn(arr));
